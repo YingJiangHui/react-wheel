98b3def9e0aa20fca809a4f16f0428fb7d5fdb7d
dark <473380917@qq.com>
2020-10-18 16:02:18 +0800

    完成点击测试

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Icon from '../Icon'
+import {mount} from 'enzyme'
 
-describe('测试icon',()=>{
-  it('icon代码快照是否正确',()=>{
+describe('测试icon', () => {
+  it('icon代码快照是否正确', () => {
     const json = renderer.create(<Icon name={'pay'}/>).toJSON()
     expect(json).toMatchSnapshot()
+  })
+  it('点击icon', () => {
+    const fn = jest.fn()
+    const component = mount(<Icon name={'pay'} onClick={fn}/>)
+    component.find('svg').simulate('click')
+    expect(fn).toBeCalled()
   })
 })
