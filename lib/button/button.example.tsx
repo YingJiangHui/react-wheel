dcc7476a3984ee298383aa8bbe3af07d01d4fb8d
dark <473380917@qq.com>
2020-11-01 11:05:19 +0800

    添加Button基础样式

--- lib/button/button.example.tsx
+++ lib/button/button.example.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Button from './button';
 const ButtonExample:React.FC=()=>{
   return (<div>
-    <Button>按钮</Button>
+    <Button onClick={()=>{console.log(1)}} type='primary'>按钮</Button>
+    <Button onClick={()=>{console.log(1)}} full type='primary'>按钮</Button>
   </div>)
 }
 export default ButtonExample
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import './button.scss'
+import React from 'react';
+import './button.scss';
 import {scopedClass} from '../helper/scopedClass';
 import classes from '../helper/classes';
 
+const cs = classes;
 
-const sc = scopedClass
-const x = sc('x-button')
-interface Props {
-  type?:string
+const sc = scopedClass;
+const x = sc('x-button');
+
+interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+  type?: string,
+  full?: boolean,
 }
-const Button:React.FunctionComponent<Props> =({children,type})=>{
-  return <button className={classes(x(),x(type))}>{children}</button>
-}
-export default Button
+const Button: React.FunctionComponent<Props> = ({children,className, type, full=false, ...rest}) => {
+  const isType = Boolean(type)
+  const classes: { [key: string]: boolean } = {
+    [x()]:true,
+    [x(type)]:Boolean(x),
+    [x(type + '-full')]:isType&&full,
+    [className||'']:Boolean(className)
+  };
+  return <button {...rest} className={cs(classes,className)}>{children}</button>;
+};
+Button.defaultProps={full:false,type:''}
+export default Button;
