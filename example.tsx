97c951b6634494248d79c24510c1f15ea3b97a4b
dark <473380917@qq.com>
2020-11-01 11:08:18 +0800

    在example中添加dialog组件示例

--- example.tsx
+++ example.tsx
@@ -3,7 +3,7 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import IconExample from './lib/icon/icon.example';
 import ButtonExample from './lib/button/button.example';
-
+import DialogExample from './lib/dialog/dialog.example'
 const App: React.FC = () => {
   return (
     <Router>
@@ -11,10 +11,12 @@ const App: React.FC = () => {
       <aside>
         <Link to='/icon'>icon</Link>
         <Link to={'/button'}>button</Link>
+        <Link to={'/dialog'}>dialog</Link>
       </aside>
       <main>
         <Route component={IconExample} path={'/icon'}></Route>
         <Route component={ButtonExample} path={'/button'}></Route>
+        <Route component={DialogExample} path={'/dialog'}></Route>
       </main>
     </Router>
 
