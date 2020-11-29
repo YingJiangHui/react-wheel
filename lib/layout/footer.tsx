96ffb7c53e447f0d14ddcfb51f3ddd55f31626fc
dark <473380917@qq.com>
2020-11-29 16:37:23 +0800

    删除layout组件

--- lib/layout/footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-interface Props{
-
-}
-const Footer:React.FC<Props> = ({children})=>{
-  return (<div>{children}</div>)
-}
-export default Footer
--- lib/layout/header.tsx
+++
@@ -1,8 +0,0 @@
-import React  from 'react';
-import './header.scss'
-interface Props {
-}
-const Header:React.FC<Props> = ({children})=>{
-  return (<div className={'x-header'}>{children}</div>)
-}
-export default Header
