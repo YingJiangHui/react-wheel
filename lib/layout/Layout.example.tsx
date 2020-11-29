059b94daefeb677730b90465e172cba115601367
dark <473380917@qq.com>
2020-11-29 16:13:31 +0800

    done - layout组件

--- lib/layout/Layout.example.tsx
+++ lib/layout/Layout.example.tsx
@@ -1,16 +1,52 @@
-import React from 'react'
-import Layout from './Layout'
+import React from 'react';
+import Layout from './Layout';
 import Header from './Header';
 import Footer from './Footer';
 import Content from './Content';
-import './layout.example.scss'
-const LayoutExample = ()=>{
+import './layout.example.scss';
+import Aside from './Aside';
+
+const LayoutExample = () => {
   return (
-    <Layout className={'example-layout'}>
-      <Header className={'example-header'}>header</Header>
-      <Content className={'example-content'}>content</Content>
-      <Footer className={'example-footer'}>footer</Footer>
-    </Layout>
-  )
-}
-export default LayoutExample
+    <>
+      <div className={'example'}>
+        <Layout className={'example-layout'}>
+          <Header className={'example-header'}>header</Header>
+          <Content className={'example-content'}>content</Content>
+          <Footer className={'example-footer'}>footer</Footer>
+        </Layout>
+      </div>
+      <div className={'example'}>
+        <Layout className={'example-layout'}>
+          <Header className={'example-header'}>header</Header>
+          <Layout>
+            <Aside className={'example-aside'}>aside</Aside>
+            <Content className={'example-content'}>content</Content>
+          </Layout>
+          <Footer className={'example-footer'}>footer</Footer>
+        </Layout>
+      </div>
+      <div className={'example'}>
+        <Layout className={'example-layout'}>
+          <Header className={'example-header'}>header</Header>
+          <Layout>
+            <Content className={'example-content'}>content</Content>
+            <Aside className={'example-aside'}>aside</Aside>
+          </Layout>
+          <Footer className={'example-footer'}>footer</Footer>
+        </Layout>
+      </div>
+      <div className={'example'}>
+        <Layout className={'example-layout'}>
+          <Aside className={'example-aside'}>aside</Aside>
+          <Layout>
+            <Header className={'example-header'}>header</Header>
+            <Content className={'example-content'}>content</Content>
+            <Footer className={'example-footer'}>footer</Footer>
+          </Layout>
+        </Layout>
+      </div>
+    </>
+  );
+};
+export default LayoutExample;
--- lib/layout/Layout.tsx
+++ lib/layout/Layout.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {ReactElement} from 'react';
 import {scopedClass} from '../helper/scopedClass'
 import './layout.scss'
+import Aside from './Aside';
 interface LayoutProps{
   className?:string
+  children:ReactElement|Array<ReactElement>
 }
 const cn = scopedClass('makabaka-layout')
 const Layout:React.FC<LayoutProps> = ({children,className=''})=>{
+  let hasAside = false
+  if((children as Array<ReactElement>).length){
+    (children as Array<ReactElement>).map(node=>{
+      if(Aside === node.type){
+        hasAside = true
+      }
+    })
+  }
   return (
-    <div  className={cn('',{extra:[className]})}>{children}</div>
+    <div className={cn({hasAside,'':true},{extra:[className]})}>{children}</div>
   )
 }
 export default Layout
