340f8da9cc05e86d8771e692dc43f8e851fc7988
dark <473380917@qq.com>
2021-01-30 19:59:50 +0800

    webpack：配置webpack不同模式

--- webpack.config.doc.js
+++ webpack.config.doc.js
@@ -1,10 +1,12 @@
 const base = require('./webpack.config')
 const htmlWebpackPlugin = require('html-webpack-plugin')
-
+const path = require('path')
 module.exports = Object.assign({},base,{
-    mode: 'development',
+    mode: 'production',
+    output:{
+        path: path.resolve(__dirname, 'doc'),
+    },
     plugins: [new htmlWebpackPlugin({
         template: './example.html',
-        filename:'./example.html'
     })],
 })
