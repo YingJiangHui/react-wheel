9ccdce85114d1ea17acdb8ca776bc69cb73e43d9
Ying <473380917@qq.com>
2021-04-15 14:27:00 +0800

    修改example入口

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,8 +3,10 @@ const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = Object.assign({},base,{
     mode: 'development',
+    entry:{
+        example:'./example.tsx'
+    },
     plugins: [new htmlWebpackPlugin({
         template: './example.html',
-        filename:'./example.html'
     })],
 })
