94eaf6ce5b8900cd66eb8727425d93a442825635
Ying <473380917@qq.com>
2021-04-23 16:30:16 +0800

    update: useClassName以及测试用例

--- lib/hooks/__tests__/useClassName.unit.ts
+++ lib/hooks/__tests__/useClassName.unit.ts
@@ -1,10 +1,31 @@
-import useClassName from "../useClassName";
+import useClassName from '../useClassName';
+import {act,renderHook} from '@testing-library/react-hooks';
 
-describe('useClassName 测试用例',()=>{
-  it('可以传入多个参数转换为class名称',()=>{
-    useClassName()
-  })
-  it('可以通过布尔类型的值进行判断是否需要使用该className',()=>{})
-  it('',()=>{})
+describe('useClassName 测试用例',() => {
+  it('可以传入多个参数转换为class名称',() => {
+    const {result} = renderHook(() => useClassName({prefix: 'lll'}));
+    act(() => {
+      result.current.setClassNames('1','2','3','4');
+    });
+    expect(result.current.classNames).toBe('lll-1 lll-2 lll-3 lll-4');
+  });
+  it('可以通过布尔类型的值进行判断是否需要使用该className',() => {
+    const {result} = renderHook(() => useClassName({prefix: 'lll'}));
+    act(() => {
+      result.current.setClassNames({
+        '1': false,'2': true,'3': false,'4': true
+      });
+    });
+    
+    expect(result.current.classNames).toBe('lll-2 lll-4');
+  });
   
-})
+  it('可以传入多个参数转换为class名称',() => {
+    const {result} = renderHook(() => useClassName({prefix: 'lll',extraClassName:['a' ,'b','c']}));
+    act(() => {
+      result.current.setClassNames('1','2','3','4');
+    });
+    expect(result.current.classNames).toBe('a b c lll-1 lll-2 lll-3 lll-4');
+  });
+  
+});
--- lib/hooks/useClassName.tsx
+++ lib/hooks/useClassName.tsx
@@ -1,13 +1,23 @@
+import {useState} from 'react';
+
 interface UseClassesProps{
   prefix?: string
-  suffix?: string
-  
+  extraClassName?: (string|number)[]
 }
 
+
 const useClassName = (props:UseClassesProps={})=>{
-  const {prefix,suffix} = props
-  return {
+  const {prefix,extraClassName=[]} = props
+  const [classNames,_setClassNames] = useState<string>('')
+  function setClassNames(arg1:{[key in number|string]:boolean}):void
+  function setClassNames(arg1:string,...args:string[]):void
+  function setClassNames (arg1:{[key in number|string]:boolean}|string,...args:string[]){
+    const cns =typeof arg1 ==='object'? Object.keys(arg1).reduce((arr:(string|number)[],key)=>(arg1[key]?[...arr,key]:arr),[]):[arg1].concat(args)
+    _setClassNames(extraClassName.concat(cns.map(arg=>prefix+'-'+arg)).join(' '))
+  }
   
+  return {
+    setClassNames,classNames
   }
 }
 export default useClassName
