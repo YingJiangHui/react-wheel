b1e04ae8dbf057e79897818d15ae42bcca847e98
Ying <473380917@qq.com>
2021-04-26 18:23:44 +0800

    update: Demo组件展开隐藏

--- lib/Demo.tsx
+++ lib/Demo.tsx
@@ -1,16 +1,18 @@
-import React, {useState} from 'react'
+import React,{useEffect,useRef,useState} from 'react';
 import Highlight, { defaultProps } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/vsDark";
 import styled from "styled-components";
 import Button from './button/button';
-interface Props{
-  code:string
+import useClassName from './hooks/useClassName';
+import './demo.scss'
+
+interface DemoProps {
+  code:string,
+  description: React.ReactElement
 }
 
 const Pre = styled.pre`
   text-align: left;
-  margin: 1em 0;
-  padding: 0.5em;
   font-family: Consolas,serif;
   overflow: scroll;
 `;
@@ -30,7 +32,10 @@ const LineNo = styled.span`
 const LineContent = styled.span`
   display: table-cell;
 `;
-const CodeComponent:React.FC<Props> =({code})=>{
+interface CodeProps{
+  code:string
+}
+const CodeComponent:React.FC<CodeProps> =({code})=>{
   return  ( <Highlight {...defaultProps} theme={theme} code={code} language="jsx">
     {({ className, style, tokens, getLineProps, getTokenProps }) => (
       <Pre className={className} style={style}>
@@ -48,16 +53,31 @@ const CodeComponent:React.FC<Props> =({code})=>{
     )}
   </Highlight>)
 }
-const Demo:React.FC<Props> = ({code,children})=>{
+
+const Demo:React.FC<DemoProps> = ({code,children,description})=>{
   const [visibleCode,setVisibleCode] = useState(false)
+  const {classNames} = useClassName({prefix:'makabaka-demo'})
+  const descriptionRef = useRef<HTMLDivElement>(null)
+  const codeRef = useRef<HTMLDivElement>(null)
+  const [codeViewHeight,setCodeViewHeight] = useState(0)
+  useEffect(()=>{
+    setCodeViewHeight((descriptionRef.current?.clientHeight||0)+(codeRef.current?.clientHeight||0))
+  },[])
   return (
-    <div>
-      <div>
+    <div className={classNames({classes:['card']})}>
+      <div className={classNames({classes:['displayComponent-view']})}>
         {children}
       </div>
-      <Button type='primary' full onClick={()=>{setVisibleCode(v=>!v)}}>{visibleCode?'隐藏源代码':'查看源代码'}</Button>
-      <div>
-        {visibleCode?<CodeComponent code={code}/>:''}
+      <div style={{height:visibleCode?codeViewHeight:0}} className={classNames({map:{'display-code-view':true}})}>
+        <div ref={descriptionRef} className={classNames({classes:['description']})}>
+          {description}
+        </div>
+        <div ref={codeRef} className={classNames({classes:['code']})}>
+          <CodeComponent code={code}/>
+        </div>
+      </div>
+      <div className={classNames({classes:['visible-code-button']})}>
+        <Button type='primary' full onClick={()=>{setVisibleCode(v=>!v)}}>{visibleCode?'隐藏源代码':'查看源代码'}</Button>
       </div>
     </div>
     )
