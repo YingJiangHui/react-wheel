53b23c5f0a6e388fc7e6de9e8c4970911e186dda
Ying <473380917@qq.com>
2021-04-27 11:14:49 +0800

    update: line组件

---
+++ lib/line/line.tsx
@@ -0,0 +1,17 @@
+import React,{FC} from 'react'
+import useClassName from '../hooks/useClassName';
+import './line.scss'
+interface LineProps {
+  grayLevel?:number
+  className?:string
+}
+
+const Line:FC<LineProps> = (props)=>{
+    const {grayLevel=2,className} = props
+    const {classNames} = useClassName({prefix:'makabaka-line'})
+    return (
+    <div className={classNames({extra:[className,'makabaka-line'],classes:[`gray-level-${grayLevel}`]})} />
+    )
+}
+
+export default Line
