bf3815ce485e036e137716e3d3c54fc01961105f
Ying <473380917@qq.com>
2021-04-27 14:43:51 +0800

    update: 完善line组件例子

--- lib/line/line.example.tsx
+++ lib/line/line.example.tsx
@@ -6,9 +6,9 @@ interface LineExampleProps {
 }
 
 const LineExample: FC<LineExampleProps> = (props) => {
-  
+  const level = [10,9,8,7,6,5,4,3,2,1,2,3,4,5,6,7,8,9,10]
   return (<>
-    {[1,2,3,4,5,6,7,8,9].map((num) => <><Line grayLevel={num}/> <br/></>)}
+    {level.map((num) => <><Line grayLevel={num}/> <br/></>)}
   </>);
 };
 
