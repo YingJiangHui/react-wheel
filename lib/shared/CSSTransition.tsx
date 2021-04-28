5ab6e4d7d66197f002216dd092a56792d3e3fff7
Ying <473380917@qq.com>
2021-04-28 15:14:57 +0800

    done: CSSTransition 组件

--- lib/shared/CSSTransition.tsx
+++ lib/shared/CSSTransition.tsx
@@ -2,60 +2,62 @@ import React,{FC,useEffect,useState} from 'react';
 import withDefaults from '../utils/with-defaults';
 
 interface Props {
-  visible: boolean,
-  enterTime: number,
-  leaveTime: number,
-  clearTime: number,
-  name: string
+  visible?: boolean,
+  enterTime?: number,
+  leaveTime?: number,
+  clearTime?: number,
+  name?: string
 }
 
 const defaultProps = {
-  visible: false,enterTime: 60,leaveTime: 60,clearTime: 60,className: '',name: 'transition'
+  visible: false,enterTime: 60,leaveTime: 60,clearTime: 0,className: '',name: 'transition'
 };
 
-// classPrefix-enter 进入
-// classPrefix-enter-active 最终的过渡效果
-// classPrefix-level
-// classPrefix-level-active
+// .classPrefix-enter 进入
+// .classPrefix-enter-active 最终的过渡效果
+// .classPrefix-level
+// .classPrefix-level-active
 type CSSTransitionProps = Props&typeof defaultProps&React.AllHTMLAttributes<any>
-const CSSTransition: FC<React.PropsWithChildren<CSSTransitionProps>> = ({visible,name,enterTime,leaveTime,clearTime,children,...props}) => {
+const CSSTransition: FC<React.PropsWithChildren<CSSTransitionProps>> = ({visible,name,enterTime,leaveTime,clearTime,children,className,...props}) => {
   const [renderable,setRenderable] = useState(visible);
-  const [className,setClassName] = useState('');
+  const [classes,setClasses] = useState('');
   
   useEffect(() => {
     if (visible && !renderable) setRenderable(true);
     const status = visible ? 'enter' : 'leave';
-    setClassName(`${name}-${status}`);
     const time = visible ? enterTime : leaveTime;
+    setClasses(`${name}-${status}`);
     
     const timer = setTimeout(() => {
-      setClassName(`${name}-${status} ${name}-${status}-active`);
+      setClasses(`${name}-${status} ${name}-${status}-active`);
       clearTimeout(timer);
     },time);
-    // leave时隐藏，enter时这个定时器没有效果t(() => {
-    //     //   if (!visible) {
-    //     //     setClassName('');
-    //     //     setRenderable(false);
-    //     //   }
-    //     //   clearTimeout(clearTimer);
-    //     // },time + clearTime);
-    // const clearTimer = setTimeou
+    // leave时隐藏，enter时这个定时器没有效果
+    const clearTimer = setTimeout(() => {
+      // 设置了clearTime执行
+      if (!visible && clearTime) {
+        setClasses('');
+        setRenderable(false);
+      }
+      clearTimeout(clearTimer);
+    },time + clearTime);
     
     return () => {
+      clearTimeout(clearTimer);
       clearTimeout(timer);
     };
   },[visible,renderable]);
   if (!React.isValidElement(children) || !renderable) return null;
-  
-  return (React.cloneElement(children,{
-    ...props,className,
-    onTransitionEnd:()=>{
-      if (!visible) {
-        setClassName('');
+  return React.cloneElement(children,{
+    ...props,className: `${className} ${children.props.className} ${classes}`,onTransitionEnd: (e:TransitionEvent) => {
+      children.props.onTransitionEnd(e);
+      // 没设置clearTime时执行
+      if (!visible && !clearTime) {
+        setClasses('');
         setRenderable(false);
       }
     }
-  }));
+  });
 };
 
 export default withDefaults(CSSTransition,defaultProps);
