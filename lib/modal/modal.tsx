9520b1f9dc6f10ddd19e28ab21cb93713b7fe12d
Ying <473380917@qq.com>
2021-04-29 18:25:58 +0800

    working: modal组件

--- lib/modal/modal.tsx
+++ lib/modal/modal.tsx
@@ -1,18 +1,28 @@
 import React,{FC} from 'react';
+import ReactDOM from 'react-dom'
 import ModalAction from './modal-action';
 import ModalActions from './modal-actions';
 import ModalTitle from './modal-title';
 import ModalContent from './modal-content';
+import useModalService,{ModalContext} from './useModalService';
+import ModalWrapper from './modal-wrapper';
+import Backdrop from '../shared/backdrop';
+import usePortal from '../utils/usePortal';
 
 interface Props {
-
+  visible?: boolean
 }
 
-const defaultProps = {};
+const defaultProps = {visible: false};
 export type ModalProps = Props&typeof defaultProps&React.AllHTMLAttributes<any>
-const Modal: FC<React.PropsWithChildren<ModalProps>> = ({}) => {
-  
-  return (<></>);
+const Modal: FC<React.PropsWithChildren<ModalProps>> = ({children,visible}) => {
+  const modalService = useModalService({visible})
+  const portal = usePortal()
+  return ReactDOM.createPortal(<ModalContext.Provider value={modalService}>
+    <Backdrop width="320px" visible={modalService.visible} onClick={()=>{modalService.close()}}>
+      <ModalWrapper>{children}</ModalWrapper>
+    </Backdrop>
+  </ModalContext.Provider>,portal);
 };
 type ModalComponent<P = {}> = React.FC<P>&{
   Title: typeof ModalTitle,Action: typeof ModalAction,Actions: typeof ModalActions
